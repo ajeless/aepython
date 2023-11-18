ec5a52fcac47ad73028af45db089bbc581932620
ajeless <ajeless@gmail.com>
2023-11-18 13:57:18 -0500

    Add chrome options and rich comments

--- browser_automation/2.1_scraping_simple_text_with_selenium.py
+++ browser_automation/2.1_scraping_simple_text_with_selenium.py
@@ -1,27 +1,52 @@
+# Import necessary modules from selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import time
 
-# Initialize Chrome options
-chrome_options = Options()
+def get_driver():
+    # Initialize an instance of ChromeOptions. This class helps in managing options specific to ChromeDriver.
+    chrome_options = Options()
 
-# Set various options
-chrome_options.add_argument("--disable-infobars")
-chrome_options.add_argument("--start-maximized")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
+    # Adding various arguments to ChromeOptions to customize the browser's behavior:
 
-driver = webdriver.Chrome(
-    service=ChromeService(ChromeDriverManager().install()), options=chrome_options
-)
+    # Disables the infobar that shows messages like "Chrome is being controlled by automated test software".
+    chrome_options.add_argument("--disable-infobars")
 
-# Now you can use `driver` to navigate and interact with web pages
-driver.get("https://www.example.com")
+    # Opens the browser in maximized mode.
+    chrome_options.add_argument("--start-maximized")
 
-# ... perform actions on the web page ...
+    # Prevents Chrome from using /dev/shm for shared memory, which can cause crashes in certain environments.
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    # Disables the sandbox security feature in Chrome. Note: This can make your browser less secure.
+    chrome_options.add_argument("--no-sandbox")
+
+    # Disables a Chrome feature that detects automation tools controlling the browser, making automation less detectable.
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+
+    # Ignores SSL certificate errors. Useful for testing on sites with self-signed certificates.
+    chrome_options.add_argument("--ignore-certificate-errors")
+
+    # Excludes the switch that enables automation, helping to make the browser automation less detectable.
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+
+    # Initialize the Chrome driver with the specified options.
+    # ChromeDriverManager automatically downloads the driver binary and sets its path.
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()), options=chrome_options
+    )
+    return driver
+
+# Call the get_driver function to initialize the Chrome WebDriver with the specified options.
+driver = get_driver()
+
+# Use the `driver` to navigate to a web page.
+driver.get("https://automated.pythonanywhere.com")
+
+# Wait for 15 seconds. This is useful for observing what happens on the browser, especially during testing.
 time.sleep(15)
 
-# Close the driver
+# Close the browser and quit the driver session. This is important to free up system resources and avoid potential leaks.
 driver.quit()
