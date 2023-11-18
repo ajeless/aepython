aeb23d6e68553329ff25712dc4d4efc54983b9e7
ajeless <ajeless@gmail.com>
2023-11-18 17:51:21 -0500

    Update

--- browser_automation/2.1_scraping_simple_text_with_selenium.py
+++ browser_automation/2.1_scraping_simple_text_with_selenium.py
@@ -6,8 +6,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 from contextlib import contextmanager
+import time
 
 
 # Defining a context manager to handle the setup and teardown of the Selenium WebDriver.
--- browser_automation/2.2_automate_login_process.py
+++ browser_automation/2.2_automate_login_process.py
@@ -1,4 +1,5 @@
 # Importing necessary modules and classes from the Selenium package and other required libraries.
+import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -72,8 +73,54 @@ def main():
         # Finding the submit button by its XPATH and clicking it.
         driver.find_element(By.XPATH, "//button[@type='submit']").click()
 
-        # Waiting for an additional 5 seconds after the actions are completed.
-        time.sleep(5)
+        print(f"{driver.current_url}")
+
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.LINK_TEXT, "Home"))
+        )
+
+        driver.find_element(By.LINK_TEXT, "Home").click()
+
+        # Looping 10 times to demonstrate repeated actions on a web page.
+        for i in range(10):
+            # Using WebDriverWait to wait for a specific element to become visible.
+            # This is a more robust method compared to static waiting, as it adapts to different loading times.
+            WebDriverWait(driver, 10).until(
+                EC.visibility_of_element_located(
+                    (By.CSS_SELECTOR, "#displaytimer .text-success")
+                )
+            )
+
+            # Finding an element by its CSS selector and printing its text content.
+            average_world_temperature = driver.find_element(
+                By.CSS_SELECTOR, "#displaytimer .text-success"
+            ).text
+
+            # Create a logger
+            logger = logging.getLogger(__name__)
+
+            # Set the log level
+            logger.setLevel(logging.INFO)
+
+            # Create a handler
+            handler = logging.FileHandler("my_log.log")
+
+            # Set the log format
+            formatter = logging.Formatter(
+                "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+            )
+            handler.setFormatter(formatter)
+
+            # Add the handler to the logger
+            logger.addHandler(handler)
+
+            # Log a message
+            logger.info(f"{average_world_temperature}")
+
+            # Pausing the script for 2 seconds before continuing the loop.
+            time.sleep(5)
+
+        print(f"{driver.current_url}")
 
 
 # This conditional statement checks if the script is being run directly (not imported as a module).
